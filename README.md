b8770a0fce1f416cfee384c0b4a8b6397940def5
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-11-08 19:18:22 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<center>
+    <h1>python-telegram-word-counter</h1>
+</center>
+
+Count occurences of word in chat history!
+
+### Usage:
+
+- Export chat history (in chat options) as JSON
+
+- Move `result.json` to this directory
+
+- Run the script:
+
+    ```shell
+    python counter.py
+    ```
+
+- Results will be saved to `words.json`
---
+++ counter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import json
+
+#with open("result_copy.json", "r") as f:
+    #chat_history = json.load(f.read())
+
+chat = json.load(open("result.json", "r", encoding="utf-8"))
+
+words = {}
+
+for message in chat["messages"]:
+    if len(message["text_entities"]) > 0:
+        for entity in message["text_entities"]:
+            for word in entity["text"].strip().split(" "):
+                word = word.lower()
+
+                try:
+                    prev = words[word]
+                except KeyError:
+                    words[word] = 0
+                
+                prev = words[word]
+
+                words[word] = prev + 1
+
+words = dict(sorted(words.items(), key=lambda item: item[1], reverse=True))
+
+with open("words.json", "w", encoding="utf-8") as f:
+    json.dump(words, f, indent=4, ensure_ascii=False)
