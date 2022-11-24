0bb2f38cc5b8adcac3c2fd6e4fd8b4e592bc9461
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-11-24 01:53:32 +0100

    Wrap as a function, parse result file as an argument

--- counter.py
+++ counter.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
 
 import json
+import sys
 
-#with open("result_copy.json", "r") as f:
-    #chat_history = json.load(f.read())
+def count_words(result):
+    words = {}
 
-chat = json.load(open("result.json", "r", encoding="utf-8"))
+    for message in result["messages"]:
+        if len(message["text_entities"]) > 0:
+            for entity in message["text_entities"]:
+                for word in entity["text"].strip().split(" "):
+                    word = word.lower()
 
-words = {}
+                    if word.startswith("http://") or word.startswith("https://"):
+                        continue
 
-for message in chat["messages"]:
-    if len(message["text_entities"]) > 0:
-        for entity in message["text_entities"]:
-            for word in entity["text"].strip().split(" "):
-                word = word.lower()
+                    try:
+                        prev = words[word]
+                    except KeyError:
+                        words[word] = 0
 
-                if word.startswith("http://") or word.startswith("https://"):
-                    continue
-
-                try:
                     prev = words[word]
-                except KeyError:
-                    words[word] = 0
-                
-                prev = words[word]
 
-                words[word] = prev + 1
+                    words[word] = prev + 1
 
-words = dict(sorted(words.items(), key=lambda item: item[1], reverse=True))
+    words = dict(sorted(words.items(), key=lambda item: item[1], reverse=True))
 
-with open("words.json", "w", encoding="utf-8") as f:
-    json.dump(words, f, indent=4, ensure_ascii=False)
+    with open("words.json", "w", encoding="utf-8") as f:
+        json.dump(words, f, indent=4, ensure_ascii=False)
+
+result = json.load(open(sys.argv[1], "r", encoding="utf-8"))
+
+count_words(result)
